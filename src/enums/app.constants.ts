15ac351e9ad0b91c9ad7faa32df679f54132a500
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2026-03-16 07:27:36 +0200

    feat: add app and route constants

---
+++ src/enums/app.constants.ts
@@ -0,0 +1,26 @@
+export class AppConstants {
+  static readonly APP_NAME = 'CuraLink';
+  static readonly APP_SLOGAN = 'where care connects';
+  
+  static readonly STATUS_ACTIVE = 'ACTIVE';
+
+  static readonly BUTTON_SUBMIT = 'SUBMIT';
+  static readonly BUTTON_CLEAR = 'Clear';
+  static readonly BUTTON_FILTER = 'Filter';
+  
+  static readonly HOSPITALS_DASHBOARD = 'Hospitals Dashboard';
+  static readonly TOTAL_STUDIES = 'TOTAL STUDIES';
+  static readonly TOTAL_PATIENTS = 'TOTAL PATIENTS';
+  static readonly TOTAL_SERIES = 'TOTAL SERIES';
+  static readonly TOTAL_IMAGES = 'TOTAL IMAGES';
+  
+  static readonly MODALITIES = 'MODALITIES';
+  static readonly DEPARTMENTS = 'DEPARTMENTS';
+  static readonly ACTIVE_DAYS = 'ACTIVE DAYS';
+  
+  static readonly SEARCH_RESULTS = 'Search Results';
+  
+  static readonly DUMMY_PAGE_TITLE = 'Coming Soon';
+  static readonly DUMMY_PAGE_DESC = 'This module is currently under development. Designs and functionality will be implemented in a future update.';
+  static readonly RETURN_TO_DASHBOARD = 'Return to Dashboard';
+}
